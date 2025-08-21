ab5a4ab6fff92ae1fdb867fd052256a13db2da96
Bryan Huynh <bryan.huynh1@ucalgary.ca>
2025-08-20 23:51:16 -0600

    remove ssl requirement

--- src/services/database-services/database-service.ts
+++ src/services/database-services/database-service.ts
@@ -1,7 +1,9 @@
-import 'reflect-metadata';
+import "reflect-metadata";
 import { Pool, PoolConfig, QueryResult } from "pg";
 import { singleton } from "tsyringe";
-import { Logger } from '../../utils/Logger';
+import { Logger } from "../../utils/Logger";
+
+export class DatabaseConnectionError extends Error {}
 
 @singleton()
 export class DatabaseService {
@@ -15,7 +17,6 @@ export class DatabaseService {
 			database: process.env.POSTGRES_DATABASE,
 			password: process.env.POSTGRES_PASSWORD,
 			port: parseInt(process.env.POSTGRES_PORT || "5432"),
-			ssl: true,
 		};
 		this.pool = new Pool(this.pgConfig);
 		this.init();
@@ -29,7 +30,19 @@ export class DatabaseService {
 		});
 	}
 
+	async testDatabase(): Promise<boolean> {
+		try {
+			const res = this.pool.query("select 1");
+			return true;
+		} catch (err) {
+			Logger.error("unable to connect to the database");
+			return false;
+		}
+	}
+
 	async execute(query: string, params?: any[]): Promise<QueryResult<any>> {
+		const dbStatus = await this.testDatabase();
+		if (!dbStatus) throw new DatabaseConnectionError();
 		Logger.info(`Executing query: ${query} with params: ${params}`);
 		// An async function automatically handles promise resolution and rejection.
 		return this.pool.query(query, params);
